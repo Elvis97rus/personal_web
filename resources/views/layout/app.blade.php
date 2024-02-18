ed93de2f0046d97072b82c487504bffed64cdce8
ArtemSh <elvis97rus@gmail.com>
2024-02-18 14:09:38 +0300

    - title changed

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{csrf_token()}}">
-    <title>Laravel</title>
+    <title>ArtemSh - Fullstack web-developer Laravel + Vue.js</title>
 
     <!-- Styles -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
